5ec41aa1d9dc52242c2dbccda2653c62e36f1be5
Fernando <fjgt2000@gmail.com>
2024-03-21 11:00:05 -0500

    se sube proyecto mvc

---
+++ UsuariosCrud/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Mvc.Controllers
+{
+  public class DashboardController : Controller
+  {
+      public IActionResult Index()
+      {
+      return View();
+      }
+  }
+  
+}
+
---
+++ UsuariosCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UsuariosCrud.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
